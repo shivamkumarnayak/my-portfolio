0a43e2361d1bce6934b7c9d5cbe930a806af761f
shivamkumarnayak <shivamkumarnayak100@gmail.com>
2023-01-01 15:11:40 +0530

    change

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -55,11 +55,11 @@ const data = [
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
+      <h5>My Recent Work </h5>
+      <h2>Portfolio</h2> 
 
       <div className="container portfolio__container">
-       {
+       {  
          data.map(({id, image, title, github, demo})=>{
            return(
             <article key={id} className="portfolio__item">
